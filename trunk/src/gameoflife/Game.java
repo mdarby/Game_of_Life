87c948f1f2442651dbf4f332f1cdbfbc84347fc7
mdarby <mdarby@28a59b95-0820-477c-846b-81b6cdf1d8e7>
2006-11-14 19:17:42 +0000

    Still have that malignant error
    git-svn-id: file:///home/subversion/Game_of_Life@7 28a59b95-0820-477c-846b-81b6cdf1d8e7

--- trunk/src/gameoflife/Game.java
+++ trunk/src/gameoflife/Game.java
@@ -1,15 +1,16 @@
 package gameoflife;
 
-import java.applet.Applet;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Random;
+import javax.swing.JFrame;
 
-public class Game extends Applet{
-  private int seed = 15;
-  private int cellSize = 1;
-  private int columns = 100 / cellSize;
+public class Game extends JFrame{
+  private static int seed = 15;
+  private static int cellSize = 1;
+  private static int columns = 150 / cellSize;
   private int currGen[][] = new int[columns][columns];
   private int nextGen[][] = new int[columns][columns];
   
@@ -37,63 +38,62 @@ public class Game extends Applet{
 	}
       }
     }
-    
+
     System.arraycopy(nextGen, 0, currGen, 0, Math.min(nextGen.length, currGen.length));
     repaint();
   }
   
   public void paint(Graphics g) {
-    g.setColor(Color.WHITE);
+    g.setColor(Color.RED);
     
-    for(int i=0; i<columns; i++){
-      for(int k=0; k<columns; k++){
-	if(currGen[i][k] == 1){
-	  g.fillRect(i*cellSize,k*cellSize,cellSize,cellSize);
+    for(int k=1; k<columns-1; k++){
+      for(int i=1; i<columns-1; i++){
+	if(currGen[k][i] == 1){
+	  g.fillRect(k*cellSize,i*cellSize,cellSize,cellSize);
 	} else{
-	  g.clearRect(i*cellSize,k*cellSize,cellSize,cellSize);
+	  g.clearRect(k*cellSize,i*cellSize,cellSize,cellSize);
 	}
       }
     }
-    life();
+   
+    life();    
   }
   
   public void update(Graphics g) {
     paint(g);
   }
   
-  public void init(){
-    setBackground(Color.black);
-    setSize(100,100);
-    Game g = new Game();
-    g.life();
-  }
-  
-  private void display(){
-    System.out.print("\n\n");
-    for(int i=0; i<columns; i++){
-      for(int k=0; k<columns; k++){
-	System.out.print(currGen[i][k] + " ");
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n\n");
-  }
-  
   public int neighbors(int col, int row){
     int total = 0;
     
-    if(col < columns && row < columns){
-      total += currGen[col-1][row-1];
-      total += currGen[col][row-1];
-      total += currGen[col+1][row-1];
-      
-      total += currGen[col-1][row];
-      total += currGen[col+1][row];
-      
-      total += currGen[col-1][row+1];
-      total += currGen[col][row+1];
-      total += currGen[col+1][row+1];
-    }
+    total += currGen[col-1][row-1];
+    total += currGen[col][row-1];
+    total += currGen[col+1][row-1];
+    
+    total += currGen[col-1][row];
+    total += currGen[col+1][row];
+    
+    total += currGen[col-1][row+1];
+    total += currGen[col][row+1];
+    total += currGen[col+1][row+1];
+    
     return total;
+  }
+  
+  public static void main(String[] args) {
+    Game g = new Game();
+    
+    g.addWindowListener(new WindowAdapter() {
+      public void windowClosing( WindowEvent evt ){
+	System.exit(0);
+      }
+    });
+    
+    g.setBackground(Color.BLACK);
+    g.pack();
+    g.life();
+    g.setVisible(true);
+    g.setSize(columns,columns);
+    g.setLocation(500,300);
   }
 }
